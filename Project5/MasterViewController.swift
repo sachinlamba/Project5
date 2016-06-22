ada5eb9783a39c45fee17dd0d55651b6aa5a6c51
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-22 18:12:43 +0530

    Add the initial project with modicfation by making clean table view project
    exit

---
+++ Project5/MasterViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MasterViewController.swift
+//  Project5
+//
+//  Created by Sachin Lamba on 22/06/16.
+//  Copyright © 2016 LambaG. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    
+    var objects = [AnyObject]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+    }
+
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // MARK: - Segues
+
+    
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row] as! NSDate
+        cell.textLabel!.text = object.description
+        return cell
+    }
+
+    
+
+    
+
+
+}
+
