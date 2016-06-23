3b34093901b4e7d88abedd44a58bae2c7ffec2e0
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-23 13:38:05 +0530

    Add the Submission field to input answer for user.

--- Project5/MasterViewController.swift
+++ Project5/MasterViewController.swift
@@ -6,18 +6,30 @@
 //  Copyright © 2016 LambaG. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class MasterViewController: UITableViewController {
 
     
-    var objects = [AnyObject]()
-
+    var objects = [String]()
+    var allWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(promptForAnswer))
+        
+        if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
+                allWords = startWords.componentsSeparatedByString("\n")
+            }
+        }
+        else {
+            allWords = ["silkworm"]
+        }
+        startgame()
     }
 
     
@@ -45,15 +57,37 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
     }
 
     
-
+    func startgame () {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(allWords) as! [String]
+        title = allWords[0]
+        objects.removeAll(keepCapacity: true)
+        tableView.reloadData()
+    }
     
+    func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .Alert)
+        ac.addTextFieldWithConfigurationHandler(nil)
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .Default) { [unowned self, ac] (action: UIAlertAction!) in
+            let answer = ac.textFields![0]
+            self.submitAnswer(answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        
+        presentViewController(ac, animated: true, completion: nil)
 
+    }
+
+    func submitAnswer(value: String ) {
+        
+    }
 
 }
 
